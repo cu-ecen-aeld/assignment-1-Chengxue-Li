603d17ee6004708c689c0fec69c7d9be3cadb47f
Cheng-Xue Li <930leo@gmail.com>
2024-08-03 17:32:42 +0800

    Fix a bug

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -10,6 +10,13 @@ fi
 # get filename and text from args
 filename="$1"
 text="$2"
+dir="$(dirname "$filename")"
+
+# if directory doesn't exist, create the directory
+if [ ! -d "$dir" ]
+then
+    mkdir -p "$dir"
+fi
 
 # write text to the file, exit with error code 1 if any error
 echo "$text" > "$filename" || {
